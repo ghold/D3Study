6fd97e4c54f220abf72ec3865a726ceed0b85580
Ghold <gholdnet@gmail.com>
2014-08-09 20:00:49 +0800

    lesson 2

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,7 @@
 ﻿var fs = require("fs"), 
 	d3 = require("d3"),
 	jsdom = require('jsdom'),
-	htmlStub = '<html><head></head><body><div id="dataviz-container"></div><script src="js/d3.min.js"></script></body></html>';
-
-var STATIC_CONFIG = require("config").static;
+	STATIC_CONFIG = require("config").static;
 
 function test (response) {
 	// body...
@@ -13,32 +11,60 @@ function test (response) {
 
 	jsdom.env({
 		features : { QuerySelector : true }
-		, html : htmlStub
+		, html : index
 		, done : function(errors, window) {
 		// this callback function pre-renders the dataviz inside the html document, then export result into a static file
  
-			var el = window.document.querySelector('#dataviz-container')
-				, body = window.document.querySelector('body')
-				, circleId = 'a2324'  // say, this value was dynamically retrieved from some database
+			var el = window.document.querySelector('#test')
+				, data = [4, 8, 15, 16, 23, 42]
+				 , body = window.document.querySelector('body')
+				// , circleId = 'a2324'  // say, this value was dynamically retrieved from some database
  
 			// generate the dataviz
-			d3.select(el)
-				.append('svg:svg')
-					.attr('width', 600)
-					.attr('height', 300)
-					.append('circle')
-						.attr('cx', 300)
-						.attr('cy', 150)
-						.attr('r', 30)
-						.attr('fill', '#26963c')
-						.attr('id', circleId) // say, this value was dynamically retrieved from some database
+			// d3.select(el)
+			// 	.append('svg:svg')
+			// 		.attr('width', 600)
+			// 		.attr('height', 300)
+			// 		.append('circle')
+			// 			.attr('cx', 300)
+			// 			.attr('cy', 150)
+			// 			.attr('r', 30)
+			// 			.attr('fill', '#26963c')
+			// 			.attr('id', circleId) // say, this value was dynamically retrieved from some database
+
+			var width = 420,
+    			barHeight = 20;
+
+			var x = d3.scale.linear()
+			    .domain([0, d3.max(data)])
+			    .range([0, width]);
+
+			var chart = d3.select(el)
+			    .attr("width", width)
+			    .attr("height", barHeight * data.length);
+
+			var bar = chart.selectAll("g")
+			    .data(data)
+			  .enter().append("g")
+			    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+			bar.append("rect")
+			    .attr("width", x)
+			    .attr("height", barHeight - 1);
+
+			bar.append("text")
+			    .attr("x", function(d) { return x(d) - 3; })
+			    .attr("y", barHeight / 2)
+			    .attr("dy", ".35em")
+			    .text(function(d) { return d; });
+
  
 			// make the client-side script manipulate the circle at client side)
-			var clientScript = "d3.select('#" + circleId + "').transition().delay(1000).attr('fill', '#f9af26')"
+			// var clientScript = "d3.select('#" + circleId + "').transition().delay(1000).attr('fill', '#f9af26')"
  
-			d3.select(body)
-				.append('script')
-					.html(clientScript)
+			// d3.select(body)
+			// 	.append('script')
+			// 		.html(clientScript)
  
 			// save result in an html file, we could also keep it in memory, or export the interesting fragment into a database for later use
 			var svgsrc = window.document.innerHTML
