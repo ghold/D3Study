0dad68ed47c8ec162db6de70e28083e223718336
Ghold <gholdnet@gmail.com>
2014-08-10 18:27:46 +0800

    lesson1 end

--- lessons/1/requestHandlers.js
+++ lessons/1/requestHandlers.js
@@ -32,41 +32,76 @@ function test (response) {
 			// 			.attr('fill', '#26963c')
 			// 			.attr('id', circleId) // say, this value was dynamically retrieved from some database
 
-			var width = 960,
-    			height = 500;
+			var margin = {top: 20, right: 30, bottom: 30, left:40},
+				width = 960 - margin.left - margin.right,
+				height = 500 - margin.top - margin.bottom;
+
+    		var x = d3.scale.ordinal()
+    			.rangeRoundBands([0, width], .1);
 
 			var y = d3.scale.linear()
 			    .range([height, 0]);
 
-			var chart = d3.select(el)
-			    .attr("width", width)
-			    .attr("height", height);
+			var xAxis = d3.svg.axis()
+				.scale(x)
+				.orient("bottom");
+			
+			var yAxis = d3.svg.axis()
+				.scale(y)
+				.orient("left")
+				.ticks(10, "%");
 
-			d3.tsv("data.tsv", type, function(error, data){
+			var chart = d3.select(el)
+			    .attr("width", width + margin.left + margin.right)
+			    .attr("height", height + margin.top + margin.bottom)
+			    .append("g")
+			    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+			fs.readFile("./lessons/1/data.tsv", "utf-8", function(err, data){
+				
+				data = d3.tsv.parse(data,type);
+
+				x.domain(data.map(function(d){ return d.name; }));				
 				y.domain([0, d3.max(data, function(d){return d.value})]);
 
-				var barWidth = width / data.length;
+				chart.append("g")
+					.attr("class", "x axis")
+					.attr("transform", "translate(0," + height + ")")
+					.call(xAxis);
+					
 
-				var bar = chart.selectAll("g")
+				chart.append("g")
+					.attr("class", "y axis")
+					.call(yAxis)
+					.append("text")
+      				.attr("transform", "rotate(-90)")
+      				.attr("y", 6)
+      				.attr("dy", ".71em")
+      				.style("text-anchor", "end")
+      				.text("Frequency");
+
+				chart.selectAll(".bar")
 					.data(data)
-					.enter().append("g")
-					.attr("transform", function(d, i) { return "translate(" + i * barWeight + ",0)"; });
+					.enter().append("rect")
+					.attr("class", "bar")
+					.attr("x", function(d) { return x(d.name);})
+					.attr("y", function(d) { return y(d.value);})
+					.attr("height", function(d) {return height - y(d.value);})
+					.attr("width", x.rangeBand());
 
-				bar.append("rect")
-				    .attr("y", function(d){return y(d.value) ;})
-				    .attr("height", function(d){ return height - y(d.value) ;})
-				    .attr("width", barWidth - 1);
+				var svgsrc = window.document.innerHTML
 
-			    bar.append("text")
-				    .attr("x", barWidth / 2)
-				    .attr("y", function(d) {return y(d.value) + 3;})
-				    .attr("dy", ".75em")
-				    .text(function(d) { return d.value; });
+				response.writeHead(200, {"Content-Type": "text/html"});  
+				response.write(svgsrc); 
+				response.end(); 
 			});
 
 			function type(d) {
 				d.value = +d.value;
+				return d;
 			}
+
+			
 
 			
 
@@ -79,11 +114,7 @@ function test (response) {
 			// 		.html(clientScript)
  
 			// save result in an html file, we could also keep it in memory, or export the interesting fragment into a database for later use
-			var svgsrc = window.document.innerHTML
-
-			response.writeHead(200, {"Content-Type": "text/html"});  
-			response.write(svgsrc); 
-			response.end(); 
+			
 		} // end jsDom done callback
 	})
 	// no semi-column was harmed during this development
